40b5a738df97400b63143a72de03ef5cc4615320
msgawade90 <msgawade90@gmail.com>
2018-04-02 16:24:10 +0800

    Task4

--- src/test/java/com/epam/AdditionTest.java
+++ src/test/java/com/epam/AdditionTest.java
@@ -40,4 +40,9 @@ class AdditionTest {
 	    public final void whenNewLinesNoExceptionThrow() {
 	        Assert.assertEquals(1+2+3, Addition.add("1\n2,3"));
 	    }
+	    @Test
+	    public final void whenDelimitersNoExceptionThrow() {
+	        Assert.assertEquals(1+2+3, Addition.add("//;\n1;2;3"));
+	    }
+	    
 }
